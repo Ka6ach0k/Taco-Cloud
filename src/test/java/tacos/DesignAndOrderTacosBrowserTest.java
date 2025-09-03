a488dac8748d8490b114d1642d31628af380e11d
Kabachok <caehka0nakypen@gmail.com>
2025-09-03 23:39:49 +0300

    Написан браузерный тест для процесса создания и заказа тако

---
+++ src/test/java/tacos/DesignAndOrderTacosBrowserTest.java
@@ -0,0 +1,242 @@
+package tacos;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DesignAndOrderTacosBrowserTest {
+
+    @LocalServerPort
+    private int port;
+
+    private static HtmlUnitDriver browser;
+
+    @Autowired
+    TestRestTemplate rest;
+
+    @BeforeAll
+    public static void setup() {
+        browser = new HtmlUnitDriver();
+        browser.manage()
+                .timeouts()
+                .implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterAll
+    public static void closeBrowser() {
+        browser.quit();
+    }
+
+    @Test
+    public void testDesignATacoPage_HappyPath() throws Exception {
+        browser.get(homePageUrl());
+        clickDesignATaco();
+        assertDesignPageElements();
+        buildAndSubmitATaco("Basic Taco", "FLTO", "GRBF", "CHED", "TMTO", "SLSA");
+        clickBuildAnotherTaco();
+        buildAndSubmitATaco("Another Taco", "COTO", "CARN", "JACK", "LETC", "SRCR");
+        fillInAndSubmitOrderForm();
+        Assertions.assertThat(browser.getCurrentUrl()).isEqualTo(homePageUrl());
+    }
+
+    @Test
+    public void testDesignATacoPage_EmptyOrderInfo() throws Exception {
+        browser.get(homePageUrl());
+        clickDesignATaco();
+        assertDesignPageElements();
+        buildAndSubmitATaco("Basic Taco", "FLTO", "GRBF", "CHED", "TMTO", "SLSA");
+        submitEmptyOrderForm();
+        fillInAndSubmitOrderForm();
+        Assertions.assertThat(browser.getCurrentUrl()).isEqualTo(homePageUrl());
+    }
+
+    @Test
+    public void testDesignATacoPage_InvalidOrderInfo() throws Exception {
+        browser.get(homePageUrl());
+        clickDesignATaco();
+        assertDesignPageElements();
+        buildAndSubmitATaco("Basic Taco", "FLTO", "GRBF", "CHED", "TMTO", "SLSA");
+        submitInvalidOrderForm();
+        fillInAndSubmitOrderForm();
+        Assertions.assertThat(browser.getCurrentUrl()).isEqualTo(homePageUrl());
+    }
+
+    /// Browser test action methods
+
+    private void buildAndSubmitATaco(String name, String... ingredients) {
+        assertDesignPageElements();
+
+        for (String ingredient : ingredients)
+            browser.findElement(By.cssSelector("input[value='" + ingredient + "']")).click();
+        browser.findElement(By.cssSelector("input#name")).sendKeys(name);
+        browser.findElement(By.cssSelector("form")).submit();
+    }
+
+    private void assertDesignPageElements() {
+        Assertions.assertThat(browser.getCurrentUrl()).isEqualTo(designPageUrl());
+        List<WebElement> ingredientGroups = browser.findElements(By.className("ingredient-group"));
+        Assertions.assertThat(ingredientGroups.size()).isEqualTo(5);
+
+        WebElement wrapGroup = browser.findElement(By.cssSelector("div.ingredient-group#wraps"));
+        List<WebElement> wraps = wrapGroup.findElements(By.tagName("div"));
+        Assertions.assertThat(wraps.size()).isEqualTo(2);
+        assertIngredient(wrapGroup, 0, "FLTO", "Flour Tortilla");
+        assertIngredient(wrapGroup, 1, "COTO", "Corn Tortilla");
+
+        WebElement proteinGroup = browser.findElement(By.cssSelector("div.ingredient-group#proteins"));
+        List<WebElement> proteins = proteinGroup.findElements(By.tagName("div"));
+        Assertions.assertThat(proteins.size()).isEqualTo(2);
+        assertIngredient(proteinGroup, 0, "GRBF", "Ground Beef");
+        assertIngredient(proteinGroup, 1, "CARN", "Carnitas");
+
+        WebElement cheeseGroup = browser.findElement(By.cssSelector("div.ingredient-group#cheeses"));
+        List<WebElement> cheeses = cheeseGroup.findElements(By.tagName("div"));
+        Assertions.assertThat(cheeses.size()).isEqualTo(2);
+        assertIngredient(cheeseGroup, 0, "CHED", "Cheddar");
+        assertIngredient(cheeseGroup, 1, "JACK", "Monterrey Jack");
+
+        WebElement veggieGroup = browser.findElement(By.cssSelector("div.ingredient-group#veggies"));
+        List<WebElement> veggies = veggieGroup.findElements(By.tagName("div"));
+        Assertions.assertThat(veggies.size()).isEqualTo(2);
+        assertIngredient(veggieGroup, 0, "TMTO", "Diced Tomatoes");
+        assertIngredient(veggieGroup, 1, "LETC", "Lettuce");
+
+        WebElement sauceGroup = browser.findElement(By.cssSelector("div.ingredient-group#sauces"));
+        List<WebElement> sauces = sauceGroup.findElements(By.tagName("div"));
+        Assertions.assertThat(sauces.size()).isEqualTo(2);
+        assertIngredient(sauceGroup, 0, "SLSA", "Salsa");
+        assertIngredient(sauceGroup, 1, "SRCR", "Sour Cream");
+    }
+
+    private void fillInAndSubmitOrderForm() {
+        Assertions.assertThat(browser.getCurrentUrl()).startsWith(orderDetailsPageUrl());
+        fillField("input#deliveryName", "Ima Hungry");
+        fillField("input#deliveryStreet", "1234 Culinary Blvd.");
+        fillField("input#deliveryCity", "Foodsville");
+        fillField("input#deliveryState", "CO");
+        fillField("input#deliveryZip", "81019");
+        fillField("input#ccNumber", "4111111111111111");
+        fillField("input#ccExpiration", "10/23");
+        fillField("input#ccCVV", "123");
+        browser.findElement(By.cssSelector("form")).submit();
+    }
+
+    private void submitEmptyOrderForm() {
+        Assertions.assertThat(browser.getCurrentUrl()).isEqualTo(currentOrderDetailsPageUrl());
+        browser.findElement(By.cssSelector("form")).submit();
+
+        Assertions.assertThat(browser.getCurrentUrl()).isEqualTo(orderDetailsPageUrl());
+
+        List<String> validationErrors = getValidationErrorsTexts();
+        Assertions.assertThat(validationErrors.size()).isEqualTo(9);
+        Assertions.assertThat(validationErrors).containsExactlyInAnyOrder(
+                "Please correct the problems below and resubmit.",
+                "Delivery name is required",
+                "Street is required",
+                "City is required",
+                "State is required",
+                "Zip code is required",
+                "Not a valid credit card number",
+                "Must be formatted MM/YY",
+                "Invalid CVV"
+        );
+    }
+
+    private void submitInvalidOrderForm() {
+        Assertions.assertThat(
+                browser.getCurrentUrl().startsWith(orderDetailsPageUrl())
+        );
+
+        fillField("input#deliveryName", "I");
+        fillField("input#deliveryStreet", "1");
+        fillField("input#deliveryCity", "F");
+        fillField("input#deliveryState", "C");
+        fillField("input#deliveryZip", "8");
+        fillField("input#ccNumber", "1234432112344322");
+        fillField("input#ccExpiration", "14/91");
+        fillField("input#ccCVV", "1234");
+
+        browser.findElement(By.cssSelector("form")).submit();
+
+        Assertions.assertThat(browser.getCurrentUrl()).isEqualTo(orderDetailsPageUrl());
+
+        List<String> validationErrors = getValidationErrorsTexts();
+        Assertions.assertThat(validationErrors.size()).isEqualTo(4);
+        Assertions.assertThat(validationErrors).containsExactlyInAnyOrder(
+                "Please correct the problems below and resubmit.",
+                "Not a valid credit card number",
+                "Must be formatted MM/YY",
+                "Invalid CVV"
+        );
+    }
+
+    private List<String> getValidationErrorsTexts() {
+        List<WebElement> validationErrorsElement = browser.findElements(By.className("validationError"));
+        List<String> validationErrors = validationErrorsElement.stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
+        return validationErrors;
+    }
+
+    private void fillField(String fieldName, String value) {
+        WebElement field = browser.findElement(By.cssSelector(fieldName));
+        field.clear();
+        field.sendKeys(value);
+    }
+
+    void assertIngredient(WebElement ingredientGroup,
+                          int ingredientIdx, String id, String name) {
+        List<WebElement> proteins = ingredientGroup.findElements(By.tagName("div"));
+        WebElement ingredient = proteins.get(ingredientIdx);
+
+        Assertions.assertThat(
+                        ingredient.findElement(By.tagName("input")).getAttribute("value"))
+                .isEqualTo(id);
+        Assertions.assertThat(
+                        ingredient.findElement(By.tagName("span")).getText())
+                .isEqualTo(name);
+    }
+
+    private void clickDesignATaco() {
+        Assertions.assertThat(browser.getCurrentUrl())
+                .startsWith(homePageUrl());
+        browser.findElement(By.cssSelector("a[id='design']")).click();
+    }
+
+    private void clickBuildAnotherTaco() {
+        Assertions.assertThat(browser.getCurrentUrl())
+                .startsWith(orderDetailsPageUrl());
+        browser.findElement(By.cssSelector("a[id='another']")).click();
+    }
+
+    /// Url helper
+
+    public String homePageUrl() {
+        return "http://localhost:" + port + "/";
+    }
+
+    public String designPageUrl() {
+        return homePageUrl() + "design";
+    }
+
+    public String orderDetailsPageUrl() {
+        return homePageUrl() + "orders";
+    }
+
+    public String currentOrderDetailsPageUrl() {
+        return homePageUrl() + "orders/current";
+    }
+}
