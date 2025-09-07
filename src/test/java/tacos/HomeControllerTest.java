2a0dc3f697e82a9ec76e8d7b0dbe0590c210dd84
Kabachok <caehka0nakypen@gmail.com>
2025-09-08 01:48:16 +0300

    Исправлена ошибка отсутсвия пользователя в тесте

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import tacos.data.IngredientRepository;
 import tacos.data.OrderRepository;
@@ -33,6 +34,7 @@ public class HomeControllerTest {
     private MockMvc mockMvc;
 
     @Test
+    @WithMockUser
     public void testHomePage() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
