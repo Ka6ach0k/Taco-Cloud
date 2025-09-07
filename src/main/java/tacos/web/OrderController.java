9befa518ecb0ba0aa4f91fbb11b99620112005ff
Kabachok <caehka0nakypen@gmail.com>
2025-09-08 01:29:54 +0300

    Добавлено авто-заполенение формы

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,15 +1,15 @@
 package tacos.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import tacos.TacoOrder;
+import tacos.User;
 import tacos.data.OrderRepository;
+import tacos.data.UserRepository;
 
 import javax.validation.Valid;
 
@@ -21,21 +21,41 @@ public class OrderController {
     private OrderRepository orderRep;
 
     @Autowired
-    public OrderController(OrderRepository orderRep) {
+    public OrderController(OrderRepository orderRep, UserRepository userRepo) {
         this.orderRep = orderRep;
     }
 
     @GetMapping("/current")
-    public String orderForm() {
+    public String orderForm(@AuthenticationPrincipal User user,
+                            @ModelAttribute TacoOrder tacoOrder) {
+        if (tacoOrder.getDeliveryName() == null) {
+            tacoOrder.setDeliveryName(user.getFullname());
+        }
+        if (tacoOrder.getDeliveryStreet() == null) {
+            tacoOrder.setDeliveryStreet(user.getStreet());
+        }
+        if (tacoOrder.getDeliveryCity() == null) {
+            tacoOrder.setDeliveryCity(user.getCity());
+        }
+        if (tacoOrder.getDeliveryState() == null) {
+            tacoOrder.setDeliveryState(user.getState());
+        }
+        if (tacoOrder.getDeliveryZip() == null) {
+            tacoOrder.setDeliveryZip(user.getZip());
+        }
+
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(@Valid TacoOrder tacoOrder, Errors errors,
+    public String processOrder(@Valid TacoOrder tacoOrder,
+                               Errors errors,
+                               @AuthenticationPrincipal User user,
                                SessionStatus sessionStatus) {
         if (errors.hasErrors())
             return "orderForm";
 
+        tacoOrder.setUser(user);
         orderRep.save(tacoOrder);
 
         sessionStatus.setComplete();
