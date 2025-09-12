96eeeecd95b41a3a1607c9c5f2ee16d67081fe48
Kabachok <caehka0nakypen@gmail.com>
2025-09-12 20:31:11 +0300

    Добавлена обработка данной страницы и расширен интерфейс запроса из БД

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,8 +1,14 @@
 package tacos.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
@@ -16,9 +22,16 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
+@ConfigurationProperties(prefix = "taco.orders")
 public class OrderController {
 
     private OrderRepository orderRep;
+
+    private int pageSize = 20;
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
 
     @Autowired
     public OrderController(OrderRepository orderRep, UserRepository userRepo) {
@@ -60,5 +73,15 @@ public class OrderController {
 
         sessionStatus.setComplete();
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String orderForUser(
+            @AuthenticationPrincipal User user, Model model) {
+
+        Pageable pageable = PageRequest.of(0, pageSize);
+        model.addAttribute("orders",
+                orderRep.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
     }
 }
