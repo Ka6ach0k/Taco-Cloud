a98aff59463da2e1d0d45f5b45d639d06d2a20ee
Kabachok <caehka0nakypen@gmail.com>
2025-09-20 00:00:05 +0300

    Убрана модифактор final у всех полей класса

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,6 +1,9 @@
 package tacos;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -12,9 +15,9 @@ import javax.persistence.Id;
 public class Ingredient {
 
     @Id
-    private final String id;
-    private final String name;
-    private final Type type;
+    private String id;
+    private String name;
+    private Type type;
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
