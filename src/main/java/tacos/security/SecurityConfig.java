4606ee651e7a2f5d62755e5c8c4fd4c85646ecbf
Kabachok <caehka0nakypen@gmail.com>
2025-09-28 17:20:28 +0300

    Добавлена запрос роли ADMIN для методом POST и DELETE в ingredient API

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package tacos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -36,6 +37,8 @@ public class SecurityConfig {
                 .antMatchers("/design", "/orders").hasRole("USER")
                 .antMatchers("/design/**", "/orders/**").hasRole("USER")
                 .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/ingredients").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/ingredients/**" ).hasRole("ADMIN")
                 .and()
                 .formLogin()
                 .loginPage("/login")
